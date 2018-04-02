4a75a86fa121f4c3d77c015238bcab7e19f61b51
SoiChienLang <38004488+SoiChienLang@users.noreply.github.com>
2018-04-02 19:05:52 +0700

    Add files via upload

---
+++ Project3/bullet.cpp
@@ -0,0 +1,16 @@
+//#include"bullet.h"
+//
+//bullet::bullet()
+//{
+//	
+//}
+//
+//bullet::drawBullet()
+//{
+//	
+//}
+//bullet::getPos(tankPlayer t)
+//{
+//	pos.X=t.pos.X;
+//	pos.Y=t.pos.Y;
+//}
